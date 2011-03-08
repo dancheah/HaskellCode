25709fbab11cd102224dde788b512c41e1740912
Daniel Cheah <me@dancheah.com>
2011-03-07 22:09:50 -0800

    Worked on more problems

--- src/99questions/ListQ1.hs
+++ src/99questions/ListQ1.hs
@@ -30,9 +30,30 @@ myReverse []     = []
 myReverse (x:xs) = myReverse(xs) ++ [x]
 
 -- Problem 6
--- Problem 6
 -- (*) Find out whether a list is a palindrome. A palindrome can 
 -- be read forward or backward; e.g. (x a m a x).
 isPalindrome list = reverse list == list
 
+-- Problem 8
+-- (**) Eliminate consecutive duplicates of list elements.
+-- If a list contains repeated elements they should be replaced with 
+-- a single copy of the element. The order of the elements should not be changed.
+compress :: (Eq a) => [a] -> [a]
+compress list = compress' (tail list) (head list)
+                where
+                    compress' [] x' = [x']
+                    compress' (x:xs) x' = if x == x' 
+                                          then (compress' xs x) 
+                                          else x' : (compress' xs x)
 
+-- Problem 9
+-- (**) Pack consecutive duplicates of list elements into sublists. 
+-- If a list contains repeated elements they should be placed in separate sublists.
+-- pack :: [a] -> [a]
+
+-- Problem 10
+-- (*) Run-length encoding of a list. Use the result of problem P09 to 
+-- implement the so-called run-length encoding data compression method. 
+-- Consecutive duplicates of elements are encoded as lists (N E) where 
+-- N is the number of duplicates of the element E.
+-- encode :: [a] -> [a]
