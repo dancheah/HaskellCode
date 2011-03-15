3891e7d6e3c13bd1d9bc0e91d5dd8e442e02737f
Daniel C <sigsegv@gmail.com>
2011-03-14 20:12:14 -0700

    Still thinking about problem 23

--- src/99questions/ListQ3.hs
+++ src/99questions/ListQ3.hs
@@ -21,7 +21,7 @@ range start end = [start .. end]
 -- Prelude System.Random>rnd_select "abcdefgh" 3 >>= putStrLn
 -- eda
 rnd_select :: [a] -> Int -> IO [a]
-rnd_select l 0 = return []
+rnd_select _ 0 = return []
 rnd_select l n = do rnd <- randomRIO (0, len)
                     let (sel, rem) = pick l rnd in
                         do newl <- rnd_select rem (n - 1)
@@ -30,3 +30,16 @@ rnd_select l n = do rnd <- randomRIO (0, len)
 
 pick :: [a] -> Int -> (a, [a])
 pick l n = (l !! n, take n l ++ drop (n + 1) l)
+
+-- This is a solution on the wiki. The way it's doing 
+-- probablities is more sophisticated. I need to convince
+-- myself that it is doing the right thing
+rnd_select1 :: [a] -> Int -> IO [a]
+rnd_select1 _ 0 = return []
+rnd_select1 (x:xs) n =
+    do r <- randomRIO (0, (length xs))
+       if r < n
+           then do
+               rest <- rnd_select1 xs (n-1)
+               return (x : rest)
+           else rnd_select1 xs n
